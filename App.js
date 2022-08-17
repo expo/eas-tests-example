fa7e0f361b2c16dba9bc423ac0db301034adb095
Dominik Sokal <dominik.sokal@swmansion.com>
2022-08-17 16:11:19 +0200

    Add dummy button

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!clicked && (
+        <Pressable testID="click-me-button" style={styles.button} onPress={() => setClicked(true)}>
+          <Text style={styles.text}>Click me</Text>
+        </Pressable>
+      )}
+      {clicked && <Text style={styles.hi}>Hi!</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  hi: {
+    fontSize: 30,
+    color: '#4630EB',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#4630EB',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
