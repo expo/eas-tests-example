125893742cd27ee5d6c92d507152f5ce0b2e89b4
Douglas Lowder <douglowder@mac.com>
2022-09-14 11:34:09 -0700

    Changes for testing debug builds (WIP)

--- e2e/homeScreen.e2e.js
+++ e2e/homeScreen.e2e.js
@@ -1,10 +1,32 @@
-describe('Home screen', () => {
-  beforeAll(async () => {
-    await device.launchApp();
-  });
+const {exec} = require('child_process');
 
+const link_url = (platform) => {
+  const url = `http://localhost:8081/index.bundle?platform=${platform}&dev=true&minify=false&disableOnboarding=1`;
+  return `com.dsokal.eas-tests-example://expo-development-client/?url=${encodeURI(url)}`;
+};
+
+const invoke_url = (platform) => {
+  if (platform === 'android') {
+    exec(`adb shell \"am start -W -a android.intent.action.VIEW -d \'\'\'${link_url(platform)}\'\'\' com.dsokal.eas-tests-example/.MainActivity\"`);
+  } else if (platform === 'ios') {
+    exec(`xcrun simctl openurl booted ${link_url(platform)}`);
+  }
+};
+
+const sleepAsync = t => new Promise(res => setTimeout(res, t));
+
+describe('Home screen', () => {
   beforeEach(async () => {
-    await device.reloadReactNative();
+    await device.launchApp({
+      newInstance: true,
+    //    launchArgs: {
+    //      EXDevMenuIsOnboardingFinished: true,
+    //    },
+      });
+    await sleepAsync(1000);
+    invoke_url(device.getPlatform());
+    await sleepAsync(1000);
+    //await device.reloadReactNative();
   });
 
   it('"Click me" button should be visible', async () => {
--- e2e/setup.js
+++ e2e/setup.js
@@ -1,5 +1,17 @@
+const { cleanup, beforeEach } = require('detox');
+const adapter = require('detox/runners/jest/adapter');
+
+beforeEach(async () => {
+  await adapter.beforeEach();
+});
+
 afterEach(async () => {
   if (testFailed) {
     await device.takeScreenshot('screenshot');
   }
 });
+
+afterAll(async () => {
+  await adapter.afterAll();
+  await cleanup();
+})
