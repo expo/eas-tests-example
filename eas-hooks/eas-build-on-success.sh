57129f5253ff084c46822b09b7f759f85e7ea2ae
Douglas Lowder <douglowder@mac.com>
2022-09-19 18:04:08 -0700

    Fix EAS hooks

--- eas-hooks/eas-build-on-success.sh
+++ eas-hooks/eas-build-on-success.sh
@@ -6,6 +6,6 @@ if [[ "$EAS_BUILD_PLATFORM" == "ios" && "$EAS_BUILD_PROFILE" == "test" ]]; then
   detox test --configuration ios.release --headless
 fi
 
-if [[ "$EAS_BUILD_PLATFORM" == "ios" && "$EAS_BUILD_PROFILE" == "testDebug" ]]; then
+if [[ "$EAS_BUILD_PLATFORM" == "ios" && "$EAS_BUILD_PROFILE" == "test_debug" ]]; then
   detox test --configuration ios.debug --headless
 fi
