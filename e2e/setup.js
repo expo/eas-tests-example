13c01673f4ebd3b100ebba234c858b05476cbefb
Douglas Lowder <douglowder@mac.com>
2022-09-26 13:20:39 -0700

    Remove TMPDIR deletion

--- e2e/setup.js
+++ e2e/setup.js
@@ -1,8 +1,6 @@
 const { cleanup } = require('detox');
 const adapter = require('detox/runners/jest/adapter');
 
-delete global.__RESOLVED_TMP_DIR_;
-
 afterAll(async () => {
   device.uninstallApp();
   await adapter.afterAll();
