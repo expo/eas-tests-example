27afe582479f7af302f42f8868b1e7b4a315f12a
Douglas Lowder <douglowder@mac.com>
2022-09-27 16:51:04 -0700

    Restore screenshots for failed tests

--- e2e/setup.js
+++ e2e/setup.js
@@ -1,6 +1,12 @@
 const { cleanup } = require('detox');
 const adapter = require('detox/runners/jest/adapter');
 
+afterEach(async () => {
+  if (testFailed) {
+    await device.takeScreenshot('screenshot');
+  }
+});
+
 afterAll(async () => {
   device.uninstallApp();
   await adapter.afterAll();
