b6b5fe4665ff1952f24b624cfa1da7bd2bc6b8d9
Dominik Sokal <dominik.sokal@swmansion.com>
2022-10-06 10:58:28 +0200

    Configure Android tests

---
+++ e2e/tests/homeScreen.e2e.js
@@ -0,0 +1,19 @@
+const { openAppForDebugBuild } = require('./utils/openAppForDebugBuild');
+
+describe('Home screen', () => {
+  beforeEach(async () => {
+    await device.launchApp({
+      newInstance: true,
+    });
+    await openAppForDebugBuild();
+  });
+
+  it('"Click me" button should be visible', async () => {
+    await expect(element(by.id('click-me-button'))).toBeVisible();
+  });
+
+  it('shows "Hi!" after tapping "Click me"', async () => {
+    await element(by.id('click-me-button')).tap();
+    await expect(element(by.text('Hi!'))).toBeVisible();
+  });
+});
